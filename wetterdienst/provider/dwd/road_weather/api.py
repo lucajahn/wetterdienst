8d3ba3c325a764796cb4d50676d5bc37ab9bac0a
Benjamin Gutzmann <gutzemann@gmail.com>
2022-06-08 18:11:16 +0200

    Adapt code to suggested implementation structure

--- wetterdienst/provider/dwd/road_weather/api.py
+++ wetterdienst/provider/dwd/road_weather/api.py
@@ -2,12 +2,19 @@
 # Copyright (c) 2018-2022, earthobservations developers.
 # Distributed under the MIT License. See LICENSE for more info.
 import logging
-from typing import Dict, List
+from enum import Enum
+from typing import Dict, List, Optional
 
 import numpy as np
 import pandas as pd
 
+from wetterdienst import Kind, Period, Provider, Resolution
+from wetterdienst.core.scalar.request import ScalarRequestCore
+from wetterdienst.core.scalar.values import ScalarValuesCore
 from wetterdienst.metadata.columns import Columns
+from wetterdienst.metadata.datarange import DataRange
+from wetterdienst.metadata.period import PeriodType
+from wetterdienst.metadata.resolution import ResolutionType
 from wetterdienst.metadata.timezone import Timezone
 from wetterdienst.provider.dwd.road_weather.download import (
     download_road_weather_observations_parallel,
@@ -24,27 +31,63 @@ from wetterdienst.provider.dwd.road_weather.metadata.period import (
 from wetterdienst.provider.dwd.road_weather.metadata.station_groups import (
     DwdObservationRoadWeatherStationGroups,
 )
-from wetterdienst.provider.dwd.road_weather.metaindex import (
-    create_meta_index_for_road_weather,
-)
 from wetterdienst.provider.dwd.road_weather.parser import parse_dwd_road_weather_data
+from wetterdienst.util.cache import CacheExpiry
 from wetterdienst.util.geo import Coordinates, derive_nearest_neighbours
+from wetterdienst.util.network import download_file
+from wetterdienst.util.parameter import DatasetTreeCore
 
 log = logging.getLogger(__name__)
 
 
-class DwdRoadWeatherObservationValues:
+class DwdRoadWeatherObservationParameter(DatasetTreeCore):
+    """
+    enumeration for different parameter/variables
+    measured by dwd road weather stations
+    """
+
+    class MINUTE_10(Enum):
+        ROAD_SURFACE_TEMPERATURE = "roadSurfaceTemperature"
+        TEMPERATURE_AIR = "airTemperature"
+        ROAD_SURFACE_CONDITION = "roadSurfaceCondition"
+        WATER_FILM_THICKNESS = "waterFilmThickness"
+        WIND_EXTREME = "maximumWindGustSpeed"
+        WIND_EXTREME_DIRECTION = "maximumWindGustDirection"
+        WIND_DIRECTION = "windDirection"
+        WIND = "windSpeed"
+        DEW_POINT = "dewpointTemperature"
+        RELATIVE_HUMIDITY = "relativeHumidity"
+        TEMPERATURE_SOIL = "soil_temperature"
+        VISIBILITY = "visibility"
+        PRECIPITATION_TYPE = "precipitationType"
+        TOTAL_PRECIPITATION = "totalPrecipitationOrTotalWaterEquivalent"
+        INTENSITY_OF_PRECIPITATION = "intensityOfPrecipitation"
+        INTENSITY_OF_PHENOMENA = "intensityOfPhenomena"
+        HORIZONTAL_VISIBILITY = "horizontalVisibility"
+        SHORT_STATION_NAME = "shortStationName"
+
+
+class DwdRoadWeatherObservationResolution(Enum):
+    MINUTE_10 = Resolution.MINUTE_10.value
+
+
+class DwdRoadWeatherObservationPeriod(Enum):
+    HISTORICAL = Period.HISTORICAL.value
+
+
+class DwdRoadWeatherObservationValues(ScalarValuesCore):
     """
     The DwdRoadWeatherObservationValues class represents a request for
     observation data from road weather stations as provided by the DWD service.
     """
+
+    _data_tz = Timezone.UTC
 
     def __init__(
         self,
         period: DwdRoadWeatherObservationPeriod = DwdRoadWeatherObservationPeriod.LATEST,
     ):
         self._tz = Timezone.GERMANY
-        self._data_tz = Timezone.UTC
         self.period = period
 
         self.metaindex = create_meta_index_for_road_weather()
@@ -167,3 +210,82 @@ class DwdRoadWeatherObservationValues:
     ) -> pd.DataFrame:
         """This does not work at the moment"""
         return data.loc[:, [param.value for param in parameters]]
+
+
+class DwdRoadWeatherObservationRequest(ScalarRequestCore):
+
+    provider = Provider.DWD
+    kind = Kind.OBSERVATION
+
+    _tz = Timezone.GERMANY
+
+    _values = None
+
+    _has_tidy_data = True
+
+    _has_datasets = True
+
+    _data_range = DataRange.FIXED
+
+    _parameter_base = DwdRoadWeatherObservationParameter
+    _unit_tree = None
+
+    _resolution_base = DwdRoadWeatherObservationResolution
+    _resolution_type = ResolutionType.FIXED
+
+    _period_base = DwdRoadWeatherObservationPeriod
+    _period_type = PeriodType.FIXED
+
+    _base_columns = list(ScalarRequestCore._base_columns)
+    _base_columns.extend(
+        (
+            Columns.STATION_GROUP.value,
+            Columns.ROAD_NAME.value,
+            Columns.ROAD_SECTOR.value,
+            Columns.ROAD_TYPE.value,
+            Columns.ROAD_SURFACE_TYPE.value,
+            Columns.ROAD_SURROUNDINGS_TYPE.value,
+        )
+    )
+
+    _endpoint = (
+        "https://www.dwd.de/DE/leistungen/opendata/help/stationen/sws_stations_xls.xlsx?__blob=publicationFile&v=11"
+    )
+
+    def _all(self) -> pd.DataFrame:
+        payload = download_file(self._endpoint, CacheExpiry.METAINDEX)
+
+        df = pd.read_excel(payload)
+
+        print(df)
+
+        df = df.rename(columns={
+            "Kennung": Columns.STATION_ID.value,
+            "GMA-Name": Columns.NAME.value,
+            "Bundesland  ": Columns.STATE.value,
+            "Straße / Fahrtrichtung": Columns.ROAD_NAME.value,
+            "Strecken-kilometer 100 m": Columns.ROAD_SECTOR.value,
+            'Streckenlage (Register "Typen")': Columns.ROAD_SURROUNDINGS_TYPE.value,
+            'Streckenbelag (Register "Typen")': Columns.ROAD_SURFACE_TYPE.value,
+            "Breite (Dezimalangabe)": Columns.LATITUDE.value,
+            "Länge (Dezimalangabe)": Columns.LONGITUDE.value,
+            "Höhe in m über NN": Columns.HEIGHT.value,
+            "GDS-Verzeichnis": Columns.STATION_GROUP.value,
+            "außer Betrieb (gemeldet)": Columns.HAS_FILE.value,
+        })
+
+        df = df.loc[df[Columns.HAS_FILE.value].isna() & df[Columns.STATION_GROUP.value] != 0, :]
+
+        df[Columns.LONGITUDE.value] = (
+            df[Columns.LONGITUDE.value].replace(",", ".", regex=True).astype(float)
+        )
+        df[Columns.LATITUDE.value] = (
+            df[Columns.LATITUDE.value].replace(",", ".", regex=True).astype(float)
+        )
+
+        return df
+
+
+if __name__ == "__main__":
+    request = DwdRoadWeatherObservationRequest("").all()
+    print(request.df)
